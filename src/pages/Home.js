79660c4997d5402561326e412eab224256493745
M. Abdul Rahman <mabdulr44@gmail.com>
2024-02-22 03:10:19 +0700

    init & prepare project

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="">
+      <h2 className="">ini cuma Home BOS !!!!</h2>
+      {/* Add content here */}
+    </div>
+  );
+};
+
+export default Home;
