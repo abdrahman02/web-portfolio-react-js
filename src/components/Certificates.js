685533a09425027b7626ffceeafc38166ccbb8e0
M. Abdul Rahman <mabdulr44@gmail.com>
2024-03-15 02:19:38 +0700

    Tambah serti

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -9,7 +9,7 @@ const Certificates = () => {
     return (
       <div className="flex justify-center">
         <img
-          src={sertifikat}
+          src={sertifikat.image}
           alt="Certificate"
           className="w-full h-auto md:max-h-96 md:w-auto"
         />
@@ -28,91 +28,29 @@ const Certificates = () => {
 
       <div className="flex flex-wrap justify-center items-center">
         <Accordion activeIndex={0} pt={{ root: { className: "lg:w-1/2" } }}>
-          <AccordionTab
-            header={
-              <span className="flex align-items-center gap-2 w-full justify-center items-center">
-                <span className="font-bold white-space-nowrap p-5">
-                  Participate as a speaker
+          {Sertifikat.map((serti, index) => (
+            <AccordionTab
+              key={index}
+              header={
+                <span className="flex align-items-center gap-2 w-full justify-center items-center">
+                  <span className="font-bold white-space-nowrap p-5">
+                    {serti.header}
+                  </span>
+                  <Badge
+                    value={serti.year}
+                    pt={{
+                      root: {
+                        className: "text-wrap w-auto h-auto ml-auto px-3",
+                      },
+                    }}
+                  />
                 </span>
-                <Badge
-                  value="2023"
-                  pt={{
-                    root: { className: "text-wrap w-auto h-auto ml-auto px-3" },
-                  }}
-                />
-              </span>
-            }
-            pt={{ headerAction: { className: "px-5" } }}
-          >
-            <p className="m-0 ps-8 pb-5 text-justify">
-              Certificate as Speaker in Basic Training for the Information
-              Systems Student Association of Malikussaleh University
-            </p>
-          </AccordionTab>
-          <AccordionTab
-            header={
-              <span className="flex align-items-center gap-2 w-full justify-center items-center">
-                <span className="font-bold white-space-nowrap p-5">
-                  Participate as a chairman
-                </span>
-                <Badge
-                  value="2022-2023"
-                  pt={{
-                    root: { className: "text-wrap w-auto h-auto ml-auto px-3" },
-                  }}
-                />
-              </span>
-            }
-            pt={{ headerAction: { className: "px-5" } }}
-          >
-            <p className="m-0 ps-8 pb-5 text-justify">
-              Certificate Malikussaleh University Information Systems Student
-              Association Malikussaleh University Information Systems Study
-              Program
-            </p>
-          </AccordionTab>
-          <AccordionTab
-            header={
-              <span className="flex align-items-center gap-2 w-full justify-center items-center">
-                <span className="font-bold white-space-nowrap p-5">
-                  Participate as a deputy chairman
-                </span>
-                <Badge
-                  value="2021-2022"
-                  pt={{
-                    root: { className: "text-wrap w-auto h-auto ml-auto px-3" },
-                  }}
-                />
-              </span>
-            }
-            pt={{ headerAction: { className: "px-5" } }}
-          >
-            <p className="m-0 ps-8 pb-5 text-justify">
-              Certificate Malikussaleh University Information Systems Student
-              Association Malikussaleh University Information Systems Study
-              Program
-            </p>
-          </AccordionTab>
-          <AccordionTab
-            header={
-              <span className="flex align-items-center gap-2 w-full justify-center items-center">
-                <span className="font-bold white-space-nowrap p-5">
-                  Participate as a participant
-                </span>
-                <Badge
-                  value="2023"
-                  pt={{
-                    root: { className: "text-wrap w-auto h-auto ml-auto px-3" },
-                  }}
-                />
-              </span>
-            }
-            pt={{ headerAction: { className: "px-5" } }}
-          >
-            <p className="m-0 ps-8 pb-5 text-justify">
-              Certificate Workshop From Anywhere PT. Solusi Data Madani
-            </p>
-          </AccordionTab>
+              }
+              pt={{ headerAction: { className: "px-5" } }}
+            >
+              <p className="m-0 ps-8 pb-5 text-justify">{serti.content}</p>
+            </AccordionTab>
+          ))}
         </Accordion>
 
         <Carousel
--- src/utils/Sertifikat.js
+++ src/utils/Sertifikat.js
@@ -2,10 +2,50 @@ import sertiPemateriTDO from "../assets/images/serti-pdf-to-png/serti-pemateri-t
 import sertiPengurusAbhinaya from "../assets/images/serti-pdf-to-png/serti-pengurus-abhinaya.png";
 import sertiPengurusProgresif from "../assets/images/serti-pdf-to-png/serti-pengurus-progresif.png";
 import sertiWorkFromAnyWay from "../assets/images/serti-pdf-to-png/serti-work-from-anyway.png";
+import sertiCRUDOOPPHP from "./../assets/images/serti-pdf-to-png/serti-oop-php.png";
+import sertiDasarPemgrogamanWeb from "./../assets/images/serti-pdf-to-png/serti-dasar-pemrograman-web.png";
+
 const Sertifikat = [
-  sertiPemateriTDO,
-  sertiPengurusAbhinaya,
-  sertiPengurusProgresif,
-  sertiWorkFromAnyWay,
+  {
+    header: "Participate as a speaker",
+    content:
+      "Certificate as Speaker in Basic Training for the Information Systems Student Association of Malikussaleh University",
+    image: sertiPemateriTDO,
+    year: "2023",
+  },
+  {
+    header: "Participate as a chairman",
+    content:
+      "Certificate Malikussaleh University Information Systems Student Association Malikussaleh University Information Systems Study Program",
+    image: sertiPengurusAbhinaya,
+    year: "2022-2023",
+  },
+  {
+    header: "Participate as a deputy chairman",
+    content:
+      "Certificate Malikussaleh University Information Systems Student Association Malikussaleh University Information Systems Study Program",
+    image: sertiPengurusProgresif,
+    year: "2021-2022",
+  },
+  {
+    header: "Participate as a participant",
+    content:
+      "Certificate Workshop From Anywhere PT. Solusi Data Madani, Snake Game With Javascript",
+    image: sertiWorkFromAnyWay,
+    year: "2023",
+  },
+  {
+    header: "Participate as a participant",
+    content:
+      "Certificate Workshop From Anywhere PT. Solusi Data Madani, CRUD Dengan OOP PHP",
+    image: sertiCRUDOOPPHP,
+    year: "2024",
+  },
+  {
+    header: "Participate as a participant",
+    content: "Certificate Dicoding Indonesia, Belajar Dasar Pemrograman Web",
+    image: sertiDasarPemgrogamanWeb,
+    year: "2024",
+  },
 ];
 export default Sertifikat;
